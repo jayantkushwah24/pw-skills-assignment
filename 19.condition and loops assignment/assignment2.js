df7d9115891f83ac3f391810e4c14272165ec137
Jayant Kushwah <jayantkushwah8@gmail.com>
2023-12-07 05:39:55 +0000

    assignment 2

---
+++ 19.condition and loops assignment/assignment2.js
@@ -0,0 +1,11 @@
+let studentMarks = 85;
+
+if(studentMarks >= 90){
+    console.log("A Grade");
+}else if(studentMarks < 90 && studentMarks >= 70){
+    console.log("B Grade");
+}else if (studentMarks < 70 && studentMarks >=50) {
+    console.log("C Grade");
+}else{
+    console.log("D Grade");
+}
